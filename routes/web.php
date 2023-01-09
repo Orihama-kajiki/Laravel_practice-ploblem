228813c58b059800fd0187756ea2e69f0a891163
Orihama-kajiki <kajiki.business@gmail.com>
2023-01-09 17:30:23 +0900

    Call to a member function update() on nullのエラー解決に向けて

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index'] );
 Route::post('/todos/create', [TodoController::class,'create']);
+Route::post('/todos/update', [TodoController::class,'edit']);
 Route::post('/todos/update', [TodoController::class,'update']);
 Route::post('/todos/delete', [TodoController::class,'delete']);
 Route::post('/todos/delete', [TodoController::class, 'remove']);
